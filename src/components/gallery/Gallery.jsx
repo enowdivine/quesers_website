a266d8a83cb7198a7a4131a06403f4b503ec0626
enowdivine <sirdivine16@gmail.com>
2024-06-21 09:23:42 +0100

    update gallery images

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import { useSnapCarousel } from "react-snap-carousel";
 import styles from "./Gallery.module.css";
 
-const images = ["/images/hero/heroImage.png", "/images/hero/heroImage.png"];
+const images = [
+  "/images/app/one.png",
+  "/images/app/two.png",
+  "/images/app/three.png",
+  "/images/app/four.png",
+  "/images/app/five.png",
+  "/images/app/six.png",
+  "/images/app/seven.png",
+  "/images/app/eight.png",
+  "/images/app/nine.png",
+  "/images/app/ten.png",
+  "/images/app/eleven.png",
+  "/images/app/twelve.png",
+  "/images/app/thirtheen.png",
+  "/images/app/fourteen.png",
+  "/images/app/fifteen.png",
+  "/images/app/sixteen.png",
+  "/images/app/seventeen.png",
+  "/images/app/eighteen.png",
+  "/images/app/nineteen.png",
+  "/images/app/twenty.png",
+  "/images/app/twentyone.png",
+  "/images/app/twentytwo.png",
+];
 
 const AdvancedCarousel = () => {
   const { scrollRef, pages, activePageIndex, next, prev, goTo } =
